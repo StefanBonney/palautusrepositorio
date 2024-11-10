99f2f2fc2952c49528b4cba7979ee33e60b1538a
Stefan <stefan.bonney@gmail.com>
2024-11-10 16:59:51 +0200

    viikko2 tehtävä10&11

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -5,18 +5,45 @@ def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players"
     response = requests.get(url).json()
 
-    print("JSON-muotoinen vastaus:")
-    print(response)
+    #print("JSON-muotoinen vastaus:")
+    #print(response)
 
     players = []
 
-    for player_dict in response:
-        player = Player(player_dict)
+    for p_dict in response:
+        player = Player(p_dict)
         players.append(player)
 
-    print("Oliot:")
+    #print("Oliot:")
 
-    for player in players:
+    #for player in players:
+        #print(player)
+
+    finnish_players = [p for p in players if p.nationality == "FIN"]
+    #for player in finnish_players:
+        #print(player)
+    finnish_player_points = [p.points for p in finnish_players]
+    finnish_player_points = sorted(finnish_player_points)
+    #print(finnish_player_points)
+
+    #---------------------------------------------------------------------------
+    finnish_players_sorted = []
+    seen_players = set()  
+
+    while finnish_player_points:
+        top_points = finnish_player_points.pop() 
+
+        top_players = [p for p in finnish_players if (p.goals + p.assists) == top_points]
+
+        for player in top_players:
+            if player not in seen_players:
+                finnish_players_sorted.append(player)
+                seen_players.add(player)
+    #---------------------------------------------------------------------------
+
+    print("Players from FIN\n")
+    for player in finnish_players_sorted:
         print(player)
+
 
 main()
--- viikko2/nhl-reader/src/player.py
+++ viikko2/nhl-reader/src/player.py
@@ -1,6 +1,11 @@
 class Player:
-    def __init__(self, dict):
-        self.name = dict['name']
+    def __init__(self, p_dict):
+        self.name = p_dict.get("name")
+        self.team = p_dict.get("team")
+        self.goals = p_dict.get("goals")
+        self.assists = p_dict.get("assists")
+        self.nationality = p_dict.get("nationality")
+        self.points = self.goals + self.assists
     
     def __str__(self):
-        return self.name
+        return f"{self.name:<20} {self.team}  {self.goals} + {self.assists} = {self.points}"
